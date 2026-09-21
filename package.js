339088604204fb88951076b62ce661b1037293b8
skbalochkalmati-a11y <skbalochkalmati@gmail.com>
2026-09-21 22:30:48 +0500

    Initialize package.json for zero-storage-apk-mirror

---
+++ package.js
@@ -0,0 +1,9 @@
+{
+  "name": "zero-storage-apk-mirror",
+  "version": "1.0.0",
+  "description": "Zero storage APK mirror hosted on Netlify",
+  "main": "index.html",
+  "dependencies": {
+    "google-play-scraper": "^9.1.1"
+  }
+}
